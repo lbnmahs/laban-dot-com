3b15ae408bd1425b35c2380978db3e007d8b9a58
LbnMahs <labanmahs2002@gmail.com>
2023-03-11 17:54:37 +0300

    Adding 3D assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
 
 const App = () => {
   return (
-    <div className="">
-     
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="z-0 relative">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
