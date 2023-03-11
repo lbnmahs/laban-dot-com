6feef54fb1ac58138fd8ef3537d07f9b86334664
LbnMahs <labanmahs2002@gmail.com>
2023-03-11 16:43:45 +0300

    Assets and Data addition

---
+++ src/assets/index.js
@@ -0,0 +1,46 @@
+import backend from './backend.png'
+import github from './github.png'
+import heroBg from './herobg.png'
+import mobile from './mobile.png'
+import web from './web.png'
+import menu from './menu.svg'
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import flutter from "./tech/flutter.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import python from "./tech/python.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+export {
+    backend,
+    github,
+    heroBg,
+    mobile,
+    web,
+    menu,
+    close,
+    css,
+    flutter,
+    figma,
+    git,
+    html,
+    javascript,
+    mongodb,
+    nodejs,
+    python,
+    reactjs,
+    redux,
+    tailwind,
+    typescript,
+    threejs,
+}
