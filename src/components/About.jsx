7eb0299a8a5f403ab476dcae1ae0e3de9ad1e6d7
LbnMahs <labanmahs2002@gmail.com>
2023-03-13 11:53:26 +0300

    About section completion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
 
-const About = () => {
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../animations/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        className='w-full green-pink-gradient rounded-[20px] shadow-card'
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      >
+        <div
+          // Tilt options
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[250px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center tracking-wide'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant}>
+        <p className={`${styles.sectionSubText}`}>Introduction</p>
+        <h2 className={`${styles.sectionHeadText}`}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[19px] max-w-3xl leading-[30px]'
+      >
+        I am a software engineer with experience in JavaScript and Python, and experience in React, Node.js, and Django. 
+        I am quick learner and collaborate closely with clients to deliver the best possible product.
+        Let us work together to build something great.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {
+          services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, 'about')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,13 +7,13 @@ module.exports = {
       colors: {
         primary: "#040303",
         secondary: "#aaa6c3",
-        tertiary: "#151030",
+        tertiary: "#191919",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0px 35px 120px -15px #222831",
       },
       screens: {
         xs: "450px",
